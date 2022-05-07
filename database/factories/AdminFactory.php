194782f22897584357b7a8b3723760be29605ac6
中村　迅 <shu.kyu0224@icloud.com>
2022-05-08 00:25:42 +0900

    JWT設定

--- database/factories/AdminFactory.php
+++ database/factories/AdminFactory.php
@@ -3,9 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
-class UserFactory extends Factory
+class AdminFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -13,8 +12,7 @@ class UserFactory extends Factory
      * @return array
      */
 
-    private static int $worker_id = 20001;
-
+    private static int $worker_id = 10001;
 
     public function definition()
     {
@@ -24,19 +22,5 @@ class UserFactory extends Factory
             'worker_id' => self::$worker_id++,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
-     */
-    public function unverified()
-    {
-        return $this->state(function (array $attributes) {
-            return [
-                'email_verified_at' => null,
-            ];
-        });
     }
 }
