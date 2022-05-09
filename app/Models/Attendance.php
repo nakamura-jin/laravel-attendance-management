c21c9efe4a9c59a626277f2ed241962b0f3684d4
中村　迅 <shu.kyu0224@icloud.com>
2022-05-09 19:51:34 +0900

    土日、祝日の判定

---
+++ app/Models/Attendance.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['year', 'month', 'days', 'start_time', 'break_time', 'end_time', 'user_id'];
+
+}
---
+++ database/migrations/2022_05_09_132513_create_attendances_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttendancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendances', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('year');
+            $table->datetime('month');
+            $table->datetime('days');
+            $table->datetime('start_time');
+            $table->datetime('break_time')->nullable();
+            $table->datetime('end_time')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendances');
+    }
+}
